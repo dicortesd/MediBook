4cc21823a84f2b027561d752c0897798c1f26049
dicortesd <80727502+dicortesd@users.noreply.github.com>
2023-09-12 17:12:27 -0500

    Add files via upload

---
+++ Scheduled_Apointment/Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ScheduleAppointment.Models
+{
+    public class Appointment
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Id_cita { get; set; }
+        public string Id_doctor { get; set;}
+        public string Id_paciente { get; set; }
+        public string? Id_consultorio { get; set; }
+        public DateTime Fecha_hora { get; set; }
+        public string Tipo_cita { get; set; }
+    }
+}
